6d0c9a86306c2a5fc368c599e84f00d9dfa77546
Eliot Henson <eliotrhenson@gmail.com>
2020-01-08 17:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 'use strict';
-function theBeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(players, instruments) {
   var allPlayers = ;
   for (var i=0; i < players.length; i++) {
 allplayers.push(players[i] + " plays " + instruments[i]); 
