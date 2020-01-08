bcdd083e9a801312365796f02e5ea120228c5644
Eliot Henson <eliotrhenson@gmail.com>
2020-01-08 16:59:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var allPlayers = ;
+  for (var i=0; i < players.length; i++) {
+allplayers.push(players[i] + " plays " + instruments[i]); 
+}
+return allPlayers; 
