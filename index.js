b8711dfd981ebdf0b70eb1b0869306452226814b
Eliot Henson <eliotrhenson@gmail.com>
2020-01-08 17:03:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ allplayers.push(players[i] + " plays " + instruments[i]);
 return allPlayers; 
 }
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to "]
+  var facts = ["He was the last Beatle to learn to drive"]
 }
